361c562fb420ab506d8981e4b2c42819eab3fcad
Spuniel <115584361+Spuniel@users.noreply.github.com>
2025-04-07 21:18:52 +0200

    obrazki, click, ui

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,4 +9,56 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'rock-paper-scissors-minus-one';
+
+  playerLeftHand: string = "leftPaperPlayer.png";
+  playerRightHand: string = "rightPaperPlayer.png";
+  opponentLeftHand: string = "leftPaperOpponent.png";
+  opponentRightHand: string = "rightPaperOpponent.png";
+
+  leftSelected: boolean = false;
+  rightSelected: boolean = false;
+
+  labelText: string = "";
+
+  leftScissorsClick(){
+    this.playerLeftHand = "leftScissorsPlayer.png"
+    this.leftSelected = true;
+    this.startGame();
+  }
+
+  rightScissorsClick(){
+    this.playerRightHand = "rightScissorsPlayer.png"
+    this.rightSelected = true;
+    this.startGame();
+  }
+
+  leftRockClick(){
+    this.playerLeftHand = "leftRockPlayer.png"
+    this.leftSelected = true;
+    this.startGame();
+  }
+
+  rightRockClick(){
+    this.playerRightHand = "rightRockPlayer.png"
+    this.rightSelected = true;
+    this.startGame();
+  }
+
+  leftPaperClick(){
+    this.playerLeftHand = "leftPaperPlayer.png"
+    this.leftSelected = true;
+    this.startGame();
+  }
+
+  rightPaperClick(){
+    this.playerRightHand = "rightPaperPlayer.png"
+    this.rightSelected = true;
+    this.startGame();
+  }
+
+  startGame(){
+    if(this.leftSelected && this.rightSelected){
+      this.labelText = "ready to play";
+    }
+  }
 }
